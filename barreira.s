18866b86b53ea9920d8b84cc1c073c247b404470
RicardoBrunetto <ra94182@uem.br>
2017-08-07 11:00:59 -0300

    Ultima versão

--- barreira.s
+++ barreira.s
@@ -3,11 +3,11 @@ shared_count: .int  0
 .text
 
 inicializar_barreira:
-  move $a0, $a1 # a0 contém o endereço da variável da barreira e a1 o número de threads
+  move $a0, $a1 # a0 = endereco da variavel da barreira / a1 = numero de threads
 jr $ra
 
 executar_barreira:
-  fi shared_count   # fetch_and_increment shared_count (atômico)
+  fi shared_count   # fetch_and_increment shared_count (atomico)
   looping:
     lw $v0, shared_count
     bne $v0, $a0, looping # segura enquanto shared_count for diferente do numero de threads
